9022531a450342eb146b6ebbef148df07305b9b9
unknown <jasper@jaspervanherpt.com>
2013-06-17 01:14:39 +0200

    Setup temp entity

---
+++ public/cms/js/controllers.js
@@ -0,0 +1,9 @@
+'use strict';
+
+function PageController($scope)
+{
+    // Temprorary statuc page.
+    $scope.page = {};
+    $scope.page.title = 'Jaztec CMS';
+    $scope.page.description = 'Temporary static page for the JaztecCMS';
+}
