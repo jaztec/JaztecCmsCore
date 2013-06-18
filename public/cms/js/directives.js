c48388e3b7ca7515249ebde4202777ca1ed6e85b
unknown <jasper@jaspervanherpt.com>
2013-06-18 22:23:54 +0200

    Filling the requested page template with the section templates

--- public/cms/js/directives.js
+++ public/cms/js/directives.js
@@ -1,11 +1,17 @@
 'use strict';
 
-jaztecCms.directive('sectionPart', function($parse) {
+jaztecCms.directive('sectionPart', function($http, $templateCache, $compile, $parse) {
     return {
         restrict: 'A',
-        transclude: true,
         link: function linkFn(scope, lElement, attrs) {
-            console.log(scope, lElement, attrs);
+            // Get the right template with this section.
+            scope.$watch(attrs, function(sectionPart) {
+                var part = '/cms/partials/sections/' + attrs.sectionPart.toLowerCase() + '.html';
+                console.log(part);
+                $http.get(part, {cache: $templateCache}).then(function(html){
+                    lElement.replaceWith($compile(html.data)(scope));
+                });
+            });
         }
     }
 });
